d4648334340793e29bd1dbb54e858c0ba3c97996
DoraLC <34760377+DoraLC@users.noreply.github.com>
2018-08-16 17:13:48 +0800

    update

--- test.ts
+++ test.ts
@@ -1,1 +1,5 @@
 // tests go here; this will not be compiled when this package is used as a library
+basic.forever(function () {
+    serial.writeNumber(ph_sensor.ph_value([604, 516], [6.86, 4.01], AnalogPin.P1))
+    serial.writeLine("")
+})
