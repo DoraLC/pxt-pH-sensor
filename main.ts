78bb40f29d23c648a6e7f6ea798a833fab82f187
DoraLC <34760377+DoraLC@users.noreply.github.com>
2019-03-18 14:33:05 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//%color=#04B404 icon="\uf0c3" block="pH_sensor"
+//%color=#04B404 icon="\uf0c3" block="pH-sensor"
 namespace ph_sensor {
     /**
      * Get raw data. For calibrating the sensor.
@@ -76,7 +76,7 @@ namespace ph_sensor {
      * This function will return the pH value by least square method.
      */
     //%blockId=pH_value
-    //%block="Calibration || raw data %data|ph %ph|pin %pin_arg"
+    //%block="Calibration raw data %data|ph %ph|pin %pin_arg"
     //%blockExternalInputs=true
     //%pin_arg.fieldEditor="gridpicker" pin_arg.fieldOptions.columns=3
     //%data.defl=[604,516] ph.defl=[6.86,4.01] pin_arg.defl=AnalogPin.P1
